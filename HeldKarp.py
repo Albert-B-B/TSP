17e05dadda7748267b84aa1a1fc8914f7ad9def4
Albert-B-B <55802313+Albert-B-B@users.noreply.github.com>
2021-05-25 09:48:40 +0200

    Update

--- HeldKarp.py
+++ HeldKarp.py
@@ -5,9 +5,36 @@ Created on Tue May 18 08:12:45 2021
 @author: Albert
 """
 
-import numpy
+from graf import *
+import itertools
+import time
+#sTPS with held-karp algorithm
+#Be warned it has to search trough n^2 * 2^n combinations
+#n is the number of verticies of the graph
 
-graf = numpy.matrix([
-    [2,2],
-    [3,3]
-    ])
+def held_karp(graph,startVertex):
+    n = graph.size
+    baseList = list(range(n))
+    baseList.remove(0)
+    costList = []
+    for i in baseList:
+        costList.append([i,graph.g[i][startVertex]])
+    print(len(costList))
+    
+    for size in range(2,n-1):
+        for combi in itertools.combinations(baseList, size):
+            for k in baseList:
+                currentBestLength = -1
+                CurrentBestSequenze = []
+                #find min
+                for m in combi:
+                    print(2)
+            print(combi)
+            
+        
+
+if __name__ == "__main__":
+    graph = graphClass()
+    graph.generateRand(500,500,4)
+    held_karp(graph,0)
+    
---
+++ TikzExample.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 21 08:06:27 2021
+
+@author: Albert
+"""
+
+import matplotlib.pyplot as plt
+import tikzplotlib 
+
+plt.plot([2,6,12])
+
+tikzplotlib.save("test.tex")
+tikzplotlib.save("test.tex")
